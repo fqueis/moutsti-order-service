c1337d79663a484780d67102a7580208845af1ec
Felipe Queis <fqueis@gmail.com>
2025-04-01 16:45:13 -0300

    feat: Implement OrderMapper for converting DTOs to entities using MapStruct

---
+++ src/main/java/info/mouts/orderservice/mapper/OrderMapper.java
@@ -0,0 +1,37 @@
+package info.mouts.orderservice.mapper;
+
+import java.util.List;
+
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Mappings;
+import org.mapstruct.factory.Mappers;
+
+import info.mouts.orderservice.domain.Order;
+import info.mouts.orderservice.domain.OrderItem;
+import info.mouts.orderservice.dto.OrderItemRequestDTO;
+import info.mouts.orderservice.dto.OrderRequestDTO;
+
+@Mapper(componentModel = "spring")
+public interface OrderMapper {
+    OrderMapper INSTANCE = Mappers.getMapper(OrderMapper.class);
+
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "order", ignore = true)
+    OrderItem toEntity(OrderItemRequestDTO dto);
+
+    List<OrderItem> toEntityList(List<OrderItemRequestDTO> dtoList);
+
+    @Mappings({
+            @Mapping(target = "id", ignore = true),
+            @Mapping(target = "status", ignore = true),
+            @Mapping(target = "total", ignore = true),
+            @Mapping(target = "idempotencyKey", ignore = true),
+            @Mapping(target = "createdAt", ignore = true),
+            @Mapping(target = "updatedAt", ignore = true),
+            @Mapping(target = "version", ignore = true),
+            @Mapping(source = "items", target = "items")
+    })
+
+    Order toEntity(OrderRequestDTO dto);
+}
