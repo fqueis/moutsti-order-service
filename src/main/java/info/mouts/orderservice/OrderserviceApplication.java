8b818387cc1a03f3061158cd607955aa52901294
Felipe Queis <fqueis@gmail.com>
2025-04-02 00:47:34 -0300

    feat: Enable Spring Caching

--- src/main/java/info/mouts/orderservice/OrderserviceApplication.java
+++ src/main/java/info/mouts/orderservice/OrderserviceApplication.java
@@ -2,8 +2,10 @@ package info.mouts.orderservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class OrderserviceApplication {
 
 	public static void main(String[] args) {
